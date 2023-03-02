7379756608362574d3bf97da77e78f6e9cc87efc
jayming66 <mjw8941@gmail.com>
2023-03-02 11:21:28 +0900

    섹션3 two sum 다시풀기

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from sec3 import sum_two_list
 
-sum_two_list.sum_two_list2()
+# sum_two_list.sum_two_list2()
+
+sum_two_list.two_sum_3()
--- sec3/sum_two_list.py
+++ sec3/sum_two_list.py
@@ -43,8 +43,35 @@ def sum_two_list2():
 
     print(result)
 
+def two_sum_3():
+    n1 = int(input())
+    l1 = list(map(int, input().split()))
+    n2 = int(input())
+    l2 = list(map(int, input().split()))
+
+    p1 = 0
+    p2 = 0
+    result = []
+
+    while (p1 < n1 and p2 < n2):
+        if l1[p1] < l2[p2]:
+            result.append(l1[p1])
+            p1 += 1
+        else:
+            result.append(l2[p2])
+            p2 += 1
+
+    if p1 == n1:
+        result = result + l2[p2:]
+    else:
+        result = result + l1[p1:]
+
+    print(result)
+    return result
+
 
 
 
     # 인덱싱을 잘 쓰자.
     # while문 조건식을 잘 쓰자.  true일 때 돌아가는 거다. False이면 끝난다.
+    # 리스트에서 +연산과 append의 차이
