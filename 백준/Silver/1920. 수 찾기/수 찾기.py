3a6de5a41d2a7c5be39afbd79dfd00f11a4d7aec
MYONG JAEWI <78201530+Jaymyong66@users.noreply.github.com>
2025-03-11 18:56:03 +0900

    [Silver IV] Title: 수 찾기, Time: 152 ms, Memory: 52272 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,28 +1,7 @@
 N = int(input())
-A = list(map(int, input().split()))
+A = set(map(int, input().split()))
 M = int(input())
 nums = list(map(int, input().split()))
 
-A.sort()
-
 for num in nums:
-    left = 0
-    # right = M - 1
-    right = len(A) - 1
-    answer = 0
-
-
-    while left <= right:
-        mid = (right + left) // 2
-
-        if num == A[mid]:
-            answer = 1
-            break
-
-        if num < A[mid]:
-            right = mid - 1
-
-        if num > A[mid]:
-            left = mid + 1
-
-    print(answer)
+    print(1 if num in A else 0)
